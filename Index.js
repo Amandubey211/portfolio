9c1df612983501db7615fa3e81ccbcaeb4f3cecb
Amandubey211 <109097090+Amandubey211@users.noreply.github.com>
2023-08-17 10:47:43 +0530

    final code

--- Index.js
+++ Index.js
@@ -1,14 +1,15 @@
 import express from "express";
 import DBConnect from "./Data_Base/DB.js";
 import FormRouter from "./MVC_/Routes/FormRouter.js";
-import cors from "cors"
+import cors from "cors";
+import path from "path";
 
 const app = express();
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "./Client/build")));
 
 app.use("/api", FormRouter);
 
@@ -16,6 +17,9 @@ app.get("/", (req, res) => {
   res.send("hii aman");
 });
 
+app.get("*",function(req,res){
+  res.sendFile(path.join(__dirname,"./Client/build/index.html"))
+})
 
 DBConnect();
 app.listen(5000, () => {
