540ecb04cd79902350004745696ab867cb5b7d97
Aman Dubey <109097090+Amandubey211@users.noreply.github.com>
2024-02-11 21:54:38 +0530

    Update FormRouter.js

--- MVC_/Routes/FormRouter.js
+++ MVC_/Routes/FormRouter.js
@@ -4,4 +4,5 @@ import { PostMessage } from "../Controller/FormController.js";
 const FormRouter = express.Router();
 
 FormRouter.route("/contact").post(PostMessage);
+FormRouter.get('/check',(req,res)=>{res.send({success:true,message:"hey there welcome to my portfolio"})})
 export default FormRouter;
